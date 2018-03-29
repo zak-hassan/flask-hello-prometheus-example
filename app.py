32bc6b51f833fcb47d7b264f6de5d125610325cb
Zak Hassan <zak.hassan1010@gmail.com>
2018-03-29 15:54:15 +0200

    Update app.py
    Added updates to the python example

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask
-from prometheus_client import start_http_server
-from prometheus_client import Counter
+from prometheus_client import start_http_server,Summary,Counter,Gauge
 
 app = Flask(__name__)
 
-REQUESTS = Counter('hello_worlds_total',
-        'Hello Worlds requested.')
+TOTAL_REQ = Counter('hello_worlds_total','Hello Worlds requested.')
+LAST_TIME = Gauge('hello_world_last_time_seconds','The last time a Hello World was served.')
 
- 
 @app.route("/")
 def hello():
-    REQUESTS.inc()
+    LAST_TIME.set_to_current_time()
+    TOTAL_REQ.inc()
     return "Hello World!"
- 
+
 if __name__ == "__main__":
     start_http_server(8000)
     app.run()
